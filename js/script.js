feb1a406183c129e7cc8fb7176594168a7809e58
Neev <introvertlearnernb@gmail.com>
2023-11-16 10:55:56 +0545

    added all the files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ------------------ typing animation ------------------- */
+var typed = new Typed(".typing",{
+    strings:["Computer Engineer","Programmer","Photographer"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
